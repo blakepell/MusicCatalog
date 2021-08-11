c16c67161304808d4e50a4e22a2ef3b712045abd
Blake Pell <blakepell@hotmail.com>
2021-08-11 13:44:11 -0400

    Test project for the SQLite editor.

---
+++ src/Avalon.Sqlite.Editor/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Avalon.Sqlite.Common;
+
+namespace Avalon.Sqlite.Editor
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void MainWindow_OnLoaded(object sender, RoutedEventArgs e)
+        {
+            await sqlite.OpenDb(@"C:\Users\blake\desktop\dsl.db");
+            await sqlite.RefreshSchemaAsync();
+        }
+    }
+}
