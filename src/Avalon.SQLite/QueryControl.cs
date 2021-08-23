35388e39985240e3e0da00b601699e497bb39075
Blake Pell <blakepell@hotmail.com>
2021-08-23 13:21:51 -0400

    Comment updates, margin on query control.

--- src/Avalon.SQLite/QueryControl.cs
+++ src/Avalon.SQLite/QueryControl.cs
@@ -38,8 +38,6 @@ using ApplicationTheme = ModernWpf.ApplicationTheme;
 using ElementTheme = ModernWpf.ElementTheme;
 
 /*
- * TODO: Selection brush to be the accent color
- * TODO: ApplyTheme in this class for preset colors.
  * TODO: Faster selection of all records
  * TODO: Vacuum
  * TODO: Export of data
@@ -47,34 +45,9 @@ using ElementTheme = ModernWpf.ElementTheme;
 
 namespace Avalon.Sqlite
 {
+
     /// <summary>
-    /// Follow steps 1a or 1b and then 2 to use this custom control in a XAML file.
-    ///
-    /// Step 1a) Using this custom control in a XAML file that exists in the current project.
-    /// Add this XmlNamespace attribute to the root element of the markup file where it is 
-    /// to be used:
-    ///
-    ///     xmlns:MyNamespace="clr-namespace:Avalon.Sqlite"
-    ///
-    ///
-    /// Step 1b) Using this custom control in a XAML file that exists in a different project.
-    /// Add this XmlNamespace attribute to the root element of the markup file where it is 
-    /// to be used:
-    ///
-    ///     xmlns:MyNamespace="clr-namespace:Avalon.Sqlite;assembly=Avalon.Sqlite"
-    ///
-    /// You will also need to add a project reference from the project where the XAML file lives
-    /// to this project and Rebuild to avoid compilation errors:
-    ///
-    ///     Right click on the target project in the Solution Explorer and
-    ///     "Add Reference"->"Projects"->[Browse to and select this project]
-    ///
-    ///
-    /// Step 2)
-    /// Go ahead and use your control in the XAML file.
-    ///
-    ///     <MyNamespace:QueryControl/>
-    ///
+    /// SQLite Query Control.
     /// </summary>
     public class QueryControl : Control, INotifyPropertyChanged
     {
